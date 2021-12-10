e1af13bdc7b5fbce069722fc474c703b50e6a5ad
Tiago Graf <tiagograf@gmail.com>
2021-12-10 12:51:38 -0800

    add get current location + lint + logging

--- web/src/components/HealthServiceAreaLookup.vue
+++ web/src/components/HealthServiceAreaLookup.vue
@@ -16,10 +16,11 @@
             <v-text-field
               data-testid="latitude"
               label="Latitude"
+              placeholder="e.g. +48.8277"            
               :rules="[
                 (v) => !!v || 'Required.',
                 (v) =>
-                  /^(\+|-)?(?:90(?:(?:\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,6})?))$/.test(
+                  /^(\+|-)?(?:90(?:(?:\.0{1,8})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,8})?))$/.test(
                     v
                   ) || 'Invalid Latitude',
               ]"
@@ -33,10 +34,11 @@
             <v-text-field
               data-testid="longitude"
               label="Longitude"
+              placeholder="e.g. -123.711"
               :rules="[
                 (v) => !!v || 'Required.',
                 (v) =>
-                  /^(\+|-)?(?:180(?:(?:\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,6})?))$/.test(
+                  /^(\+|-)?(?:180(?:(?:\.0{1,8})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,8})?))$/.test(
                     v
                   ) || 'Invalid Longitude',
               ]"
@@ -47,16 +49,24 @@
         </v-row>
         <v-row>
           <v-col>
-            <v-btn type="submit" :disabled="!isValid">
+            <v-btn type="submit" color="primary" :disabled="!isValid">
               <v-progress-circular v-if="isLoading" indeterminate />
               <span v-else>Lookup</span>
+            </v-btn>
+          </v-col>
+          <v-col>
+            <v-btn type="button" class="float-right" @click="getCurrentLocation" :disabled="!isCurrentLocationSupported">
+              <v-progress-circular v-if="isGeoLoading" indeterminate />
+              <span v-else>Get My Current Location</span>
             </v-btn>
           </v-col>
         </v-row>
         <div v-show="!!errorMessage" class="text-center">
           <v-row>
             <v-col
-              ><v-alert data-testid="errorMessage" type="error">{{ errorMessage }}</v-alert></v-col
+              ><v-alert data-testid="errorMessage" type="error">{{
+                errorMessage
+              }}</v-alert></v-col
             >
           </v-row>
         </div>
@@ -66,7 +76,10 @@
               <v-alert v-show="areaName == ' '" type="warning"
                 >Community Health Service Area <br />Not Found</v-alert
               >
-              <v-alert v-show="areaName != ' '" data-testid="successMessage" type="success"
+              <v-alert
+                v-show="areaName != ' '"
+                data-testid="successMessage"
+                type="success"
                 >Community Health Service Area: <br />{{ areaName }}</v-alert
               >
             </v-col>
@@ -90,10 +103,12 @@ export default class HealthServiceAreaLookup extends Vue {
   @Inject(Types.HealthServiceAreaLookup)
   private lookupService!: IHealthServiceArea;
 
+  isCurrentLocationSupported = window.navigator.geolocation;
   isValid = false;
   isLoading = false;
-  latitude = "+48.8277";
-  longitude = "-123.711";
+  isGeoLoading = false;
+  latitude = "";
+  longitude = "";
   areaName? = "";
   errorMessage? = "";
 
@@ -112,6 +127,19 @@ export default class HealthServiceAreaLookup extends Vue {
       this.errorMessage = response.message;
     }
     this.isLoading = false;
+  }
+
+  private getCurrentLocation() {
+    this.isGeoLoading = true;
+    window.navigator.geolocation.getCurrentPosition((position) => {
+      this.latitude = position.coords.latitude.toString();
+      this.longitude = position.coords.longitude.toString();
+      this.isGeoLoading = false;
+    }, (error) => {
+      console.error(error);
+      this.errorMessage = error.message;
+      this.isGeoLoading = false;
+    });
   }
 }
 </script>
--- web/src/services/HealthServiceArea.ts
+++ web/src/services/HealthServiceArea.ts
@@ -11,19 +11,23 @@ export default class HealthServiceArea implements IHealthServiceArea {
   private readonly API_URL: string = `${process.env.VUE_APP_API_BASE_URL}/api/location/point`;
 
   public async getAreaName(request: LookupRequest): Promise<LookupResponse> {
+    console.log("Fetching area name...");
     const result = new LookupResponse();
     try {
       const httpResponse = await axios.get<CommunityHealthServiceAreaName>(
         `${this.API_URL}/${request.latitude}/${request.longitude}`
       );
       if (httpResponse.status == 200) {
+        console.log(`Fetching area name finished`);
         result.data = httpResponse.data;
         result.status = RequestStatus.Success;
       } else {
+        console.error(`Fetching area name failed: ${httpResponse.statusText}`);
         result.status = RequestStatus.Error;
         result.message = `Error fetching Area Name: ${httpResponse.statusText}`;
       }
     } catch (e) {
+      console.error(`Fetching area name failed: ${e}`);
       result.status = RequestStatus.Error;
       result.message = `Unknown exception fetching Area Name: ${e}`;
     }
