bff3c705ebd461b71d9e5eb981361d6148c33963
Victor Ameh <Victor.ameh@tech-innovate.com>
2021-12-10 21:57:04 +0100

    updated docker-compose.yaml and code cleaning

--- api/src/location/location.router.ts
+++ api/src/location/location.router.ts
@@ -21,34 +21,25 @@ export const locationRouter = express.Router();
  * Controller Definitions
  */
 
-
 // GET location /point/:lat/:long
+locationRouter.get("/point/:lat/:long", async(req: Request, res: Response) => {
 
-locationRouter.get("/point/:lat/:long", (req: Request, res: Response) => {
-    
-    let CMNTY_HLTH_SERV_AREA_NAME: string = " ";
-    const lat = parseFloat(req.params.lat);
-    const long = parseFloat(req.params.long);
-    
-    const url = encodeURI(`https://openmaps.gov.bc.ca/geo/pub/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=pub:WHSE_ADMIN_BOUNDARIES.BCHA_CMNTY_HEALTH_SERV_AREA_SP&srsname=EPSG:4326&cql_filter=INTERSECTS(SHAPE,SRID=4326;POINT(${long}+${lat}))&propertyName=CMNTY_HLTH_SERV_AREA_CODE,CMNTY_HLTH_SERV_AREA_NAME&outputFormat=application/json`);
+    try {
+        let CMNTY_HLTH_SERV_AREA_NAME: string = " ";
 
-    axios
-        .get<GeoServerResponse>(url)
-        .then(response => {
+        const lat = parseFloat(req.params.lat);
+        const long = parseFloat(req.params.long);
 
-            if (response.data.features === undefined || response.data.features.length == 0) {
-                throw new Error("Could not fetch location!");
-            }
-            let CMNTY_HLTH_SERV_AREA_NAME = response.data.features[0].properties.CMNTY_HLTH_SERV_AREA_NAME;
+        CMNTY_HLTH_SERV_AREA_NAME = await LocationService.getLocationfromCordinates(lat, long);
+        console.log(" CMNTY_HLTH_SERV_AREA_NAME :" + CMNTY_HLTH_SERV_AREA_NAME)
+        
+        if (CMNTY_HLTH_SERV_AREA_NAME) {
             return res.status(200).send({ "CMNTY_HLTH_SERV_AREA_NAME": CMNTY_HLTH_SERV_AREA_NAME });
-        })
-        .catch(err => {
-            
-            return res.status(200).send({ "CMNTY_HLTH_SERV_AREA_NAME": " " });
-
-
-        });
-
+        }    
+    } catch (err) {
+        return res.status(200).send({ "CMNTY_HLTH_SERV_AREA_NAME": " " });
+        
+    }
 
 });
 
